9070575a5c8f9b0a602ae5609dc5f37a828f62ba
TedMartell <martellted@gmail.com>
2024-03-13 20:03:20 +0200

    adde LeafNode as a subclass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,3 +16,22 @@ class HTMLNode:
         
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+    class LeafNode(HTMLNode):
+        def __init__(self, tag, value, children, props):         
+            super().__init__(tag, value, children, props)
+            self.children = []
+
+    def to_html(self):
+        text = ""
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f"{text}{self.value}"
+        if self.props != None:
+            for prop in self.props:
+                text+= f' {prop}="{self.props[prop]}"'          
+        return f'<{self.tag}>{text}{self.value}</{self.tag}>'
+        
+
+
