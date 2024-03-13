fed56bb6644ec6289d6806cdd87b1478473a6572
TedMartell <martellted@gmail.com>
2024-03-13 20:08:24 +0200

    SMall change

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,10 +17,10 @@ class HTMLNode:
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
-    class LeafNode(HTMLNode):
-        def __init__(self, tag, value, children, props):         
-            super().__init__(tag, value, children, props)
-            self.children = []
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, children, props):         
+        super().__init__(tag, value, children, props)
+        self.children = []
 
     def to_html(self):
         text = ""
@@ -30,7 +30,7 @@ class HTMLNode:
             return f"{text}{self.value}"
         if self.props != None:
             for prop in self.props:
-                text+= f' {prop}="{self.props[prop]}"'          
+                text+= f'{prop}="{self.props[prop]}"'          
         return f'<{self.tag}>{text}{self.value}</{self.tag}>'
         
 
