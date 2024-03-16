077b032c57a3b1b387cf253e1117a0aefdee209d
TedMartell <martellted@gmail.com>
2024-03-16 18:18:46 +0200

    Added split_nodes_image

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,6 +5,7 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_image,
 )
 
 
@@ -34,7 +35,31 @@ def extract_markdown_images(text):
     
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)",text)
-        
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    if old_nodes == []:
+        return []
+    for old_node in old_nodes:
+        split_nodes = []
+        if extract_markdown_images(old_node) == []:
+            new_nodes.append(old_node)
+        images = extract_markdown_images(old_node.text)
+        remaining_text = old_node.text
+        for i in range(len(images)):
+            sections = remaining_text.split(f"![{images[i][0]}]({images[i][1]})", 1)
+            remaining_text = sections[1]
+            if sections[0] != "":
+                split_nodes.append(TextNode((sections[0].strip()), text_type_text))
+            if images[i][0] != "":
+                split_nodes.append(TextNode((images[i][0].strip()), text_type_image, images[i][1]))
+        if remaining_text != "":
+            split_nodes.append(TextNode(remaining_text.strip(), text_type_text))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+
+
 
 
 
