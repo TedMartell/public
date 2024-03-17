13de3ab9fda6aaf1a0928518ed7487e9a0c59c10
TedMartell <martellted@gmail.com>
2024-03-17 23:37:56 +0200

    added block_to_block_type and test

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
+
+
+
+
 def markdown_to_blocks(markdown):
     blocks = []
     lines = markdown.split("\n")
@@ -12,3 +24,44 @@ def markdown_to_blocks(markdown):
             blocks.append(block.strip())
             block = ""         
     return blocks
+
+def block_to_block_type(block):
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    elif block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    lines = block.split("\n")
+    all_lines_startswith_quote = True
+    for line in lines:        
+        if line.startswith(">"):
+            continue
+        else:
+            all_lines_startswith_quote = False
+            break
+    if all_lines_startswith_quote:
+        return block_type_quote
+    all_lines_startswith_unordered = True
+    for line in lines:
+        if  line.startswith("*") or line.startswith("-"):
+            continue
+        else:
+            all_lines_startswith_unordered = False
+            break
+    if all_lines_startswith_unordered:
+            return block_type_unordered_list
+    all_lines_startswith_ordered = True
+    for i, line in enumerate(lines, start=1):
+        if not line.startswith(f"{i}."):
+            all_lines_startswith_ordered = False
+            break
+    if all_lines_startswith_ordered:
+        return block_type_ordered_list
+    else:
+        return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,13 @@
 import unittest
 from markdown_blocks import (
     markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_ordered_list,
+    block_type_unordered_list,
+    block_type_quote,
 )
 
 from textnode import (
@@ -27,6 +34,21 @@ class TestInlineMarkdown(unittest.TestCase):
                   "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
                   "* This is a list item\n* This is another list item"]
         self.assertEqual(markdown_to_blocks(text), result)
+
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
 
 
 
