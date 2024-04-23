694c8acbb44663289abf294ed1eeaa03a2c94acb
TedMartell <martellted@gmail.com>
2024-04-23 04:14:20 +0300

    Adjusted main to copy directories

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
+import os
+import shutil
 from textnode import TextNode
 
-def main():
-    example = TextNode(text="whatever text", text_type="bold", url="www.whatever.com")
-    print(example)
+def copy_dir(source, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for item in os.listdir(source):
+        if os.path.isfile(os.path.join(source, item)):
+            shutil.copy(os.path.join(source, item), destination)
+        else:
+            copy_dir(os.path.join(source, item), os.path.join(destination, item))
 
-main()
+def main(): 
+    source_dir = '../static'  # Note the '/': It's '../static', not '..static'
+    destination_dir = '../public'  # Similarly, ensure it's '../public'
+    copy_dir(source_dir, destination_dir)
+    print("Copying complete!")
 
 
+if __name__ == "__main__":
+    main()
+
+
+        
